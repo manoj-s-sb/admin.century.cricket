bf1cf59fd9a79ea26241ef9b19334e17b5004358
Manojkumar <manoj@stancebeam.com>
2025-11-13 16:45:17 +0530

    Delete app directory

--- app/components/ProtectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-interface ProtectedRouteProps {
-  children: React.ReactNode;
-}
-
-export default function ProtectedRoute({ children }: ProtectedRouteProps) {
-  return <>{children}</>;
-} 
--- app/components/ui/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Dropdown } from './Dropdown';
-export { default as Input } from './Input';
-export { default as Pagination } from './Pagination';
